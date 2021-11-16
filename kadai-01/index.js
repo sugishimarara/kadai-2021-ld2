e6b5bb45bc4dd4299493aea98454684206786150
sugishimarara <shandaorara@gmail.com>
2021-11-16 12:47:43 +0000

    課題１を提出

--- kadai-01/index.js
+++ kadai-01/index.js
@@ -1,1 +1,11 @@
 console.log("おはよう")
+const firstName = "Ken"
+const familyName = "Satoh"
+console.log(firstName + familyName)
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+const dog = false
+const cat = false
+console.log(dog || cat)
+console.log(dog && cat)
+console.log(!dog && !cat)
